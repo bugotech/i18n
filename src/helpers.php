b67773fcff495e944d2ab43f51def1e4a87a5ad0
Bruno Goncalves <bugotech@gmail.com>
2017-06-20 00:25:09 -0300

    inicio

---
+++ src/helpers.php
@@ -0,0 +1,37 @@
+<?php
+
+if (! function_exists('trans')) {
+    /**
+     * Translate the given message.
+     *
+     * @param  string  $id
+     * @param  array   $replace
+     * @param  string  $locale
+     * @return \Symfony\Component\Translation\TranslatorInterface|string
+     */
+    function trans($id = null, $replace = [], $locale = null)
+    {
+        if (is_null($id)) {
+            return app('translator');
+        }
+
+        return app('translator')->trans($id, $replace, 'messages', $locale);
+    }
+}
+
+if (! function_exists('trans_choice')) {
+    /**
+     * Translates the given message based on a count.
+     *
+     * @param  string  $id
+     * @param  int|array|\Countable  $number
+     * @param  array   $parameters
+     * @param  string  $domain
+     * @param  string  $locale
+     * @return string
+     */
+    function trans_choice($id, $number, array $parameters = [], $domain = 'messages', $locale = null)
+    {
+        return app('translator')->transChoice($id, $number, $parameters, $domain, $locale);
+    }
+}
