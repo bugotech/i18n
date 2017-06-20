fa61a45dca0dc2f21367475418133ca7acfa0355
Bruno Goncalves <bugotech@gmail.com>
2017-06-20 01:16:43 -0300

    inicio

---
+++ src/I18nServiceProvider.php
@@ -0,0 +1,17 @@
+<?php namespace Bugotech\I18n;
+
+use Illuminate\Support\ServiceProvider;
+
+class I18nServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Translate
+        $this->app->instance('path.lang', $this->app->path('langs'));
+        $this->app->register('Illuminate\Translation\TranslationServiceProvider');
+
+        // Jargões
+        $this->app->instance('path.jargon', $this->app->path('jargon'));
+        $this->app->register('Bugotech\Jargon\JargonServiceProvider');
+    }
+}
