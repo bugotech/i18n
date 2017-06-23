6515fc3149d49641e301d66a03ffcc04000e4d82
Bruno Goncalves <bugotech@gmail.com>
2017-06-23 00:53:02 -0300

    path.lang

--- src/I18nServiceProvider.php
+++ src/I18nServiceProvider.php
@@ -7,7 +7,7 @@ class I18nServiceProvider extends ServiceProvider
     public function register()
     {
         // Translate
-        $this->app->instance('path.lang', __DIR__ . '/../../netforcews/i18n/langs');
+        $this->app->instance('path.lang', realpath(__DIR__ . '/../../../netforcews/i18n/langs'));
         $this->app->register('Illuminate\Translation\TranslationServiceProvider');
         //$this->loadTranslationsFrom(__DIR__ . '/../../netforcews/i18n/langs', '*');
 
